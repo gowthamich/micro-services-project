c593a258ea5109dc16ac2872040b132eab9935fc
gowthami <gowthamichirukuri@gmail.com>
2026-09-16 20:52:17 +0530

    added comments

--- service_one/app.py
+++ service_one/app.py
@@ -3,16 +3,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
+ # 1. commented out the status endpoint for service one
 @app.get("/api/greeting")
 def greeting():
     return jsonify(
         {
             "service": "service-one",
-            "message": "Hello from service one",
+            "message": "Hello from service one Gowthami",
         }
     )
 
-
+ # 2. added a new endpoint for service one
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001, debug=True)
