7eb7457b33dbf9d84ffea3e9d9e2bf52991eecf8
gowthami <gowthamichirukuri@gmail.com>
2026-09-19 10:16:07 +0530

    changes ports

--- service_one/app.py
+++ service_one/app.py
@@ -15,4 +15,4 @@ def greeting():
 
  # 2. added a new endpoint for service one
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5004, debug=True)
