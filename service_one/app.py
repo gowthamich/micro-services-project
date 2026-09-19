fe4414f8b959e986f29e74ab89a24be06a7e7d26
gowthami <gowthamichirukuri@gmail.com>
2026-09-19 10:32:00 +0530

    changed route

--- service_one/app.py
+++ service_one/app.py
@@ -12,7 +12,9 @@ def greeting():
             "message": "Hello from service one Gowthami",
         }
     )
-
+@app.route('/')
+def home():
+    return "Service One is running!"
  # 2. added a new endpoint for service one
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5004, debug=True)
